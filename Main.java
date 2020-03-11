ad3e9a7436364289b05652764f1ee6f4f6daf481
michal3489 <michal3489@gmail.com>
2020-03-11 23:23:11 +0000

    dodane case i switch

--- Main.java
+++ Main.java
@@ -2,6 +2,49 @@ import java.util.Scanner;
 
 public class lekcja1 {
     public static void main(String[] args){
+               
+            int poraRoku = 1;
+            switch (poraRoku){
+            case 1:
+                System.out.println("Wiosna");
+                break;
+            case 2:
+                System.out.println("Lato");
+                break;
+            case 3:
+                System.out.println("Jesień");
+                break;
+            case 4:
+                System.out.println("Zima");
+                break;
+                default:
+                    System.out.println("Żyjesz na innej planecie");
+                    break;
+    int a = 25;
+        int b = 10;
+        double wynik = (double)a / b;
+        int wynik2 = a % b;
+        int wynik3 = 2 + 2 * 2;
+        System.out.println(wynik);
+        System.out.println(wynik2);
+        System.out.println(wynik3);
+        a = a + b;
+        a += b;
+        Math.pow(2, 3);
+        final double sqrt = Math.sqrt(256);
+        System.out.println(Math.pow(2, 5));
+        System.out.println(sqrt);
+         int a = 5, b = 6;
+        boolean logiczna = a == b;
+        boolean logiczn2a = a >= b;
+        boolean logiczna3 = a < b && b > a;
+        boolean logiczna4 = a < b || b > a;
+        System.out.println(logiczna);
+        System.out.println(logiczn2a);
+        System.out.println(logiczna3);
+        System.out.println(logiczna4);
+
+/*
         // dodanie zmiennej char
         char firstName;
         firstName = 'b';
@@ -41,8 +84,8 @@ public class lekcja1 {
         // stałe
         int liczba = 256;
             liczba = zmByte;
+  
 
-
-
+*/
     }
 }
